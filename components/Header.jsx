e375575a517d8fc2d808520591f5dfaf67fb9c76
desperado1802 <mumun_ali@hotmail.com>
2023-03-26 20:59:02 +0300

    added home icon, add icon and profile pic at the right side of the header, and made the header sticky

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,42 +1,54 @@
 import Image from "next/image";
 import React from "react";
-import { SearchIcon } from "@heroicons/react/outline";
+import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl">
-      {/* Left */}
-      <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
-        <Image
-          alt="logo"
-          src="https://assets.website-files.com/5ee732bebd9839b494ff27cd/5f00ff5ae68abe264c4a109b_Screen%20Shot%202020-07-05%20at%2012.09.44%20AM.png"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-      <div className="h-10 w-10 relative  lg:hidden cursor-pointer">
-        <Image
-          alt="logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"
-          layout="fill"
-          className="object-contain"
-        />
-      </div>
-
-      {/* Middle */}
-
-      <div className="relative mt-1">
-        <div className="absolute top-[50%] left-2 transform translate-y-[-50%]">
-          <SearchIcon className="h-5 text-gray-500" />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-50">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto ">
+        {/* Left */}
+        <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
+          <Image
+            alt="logo"
+            src="https://techcrunch.com/wp-content/uploads/2014/06/instagram_topic.png"
+            layout="fill"
+            className="object-contain"
+          />
         </div>
-        <input
-          type="text"
-          placeholder="Search"
-          className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
-        />
-      </div>
+        <div className="h-10 w-10 relative  lg:hidden cursor-pointer">
+          <Image
+            alt="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1200px-Instagram_logo_2016.svg.png"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
 
-      {/* Right */}
+        {/* Middle */}
+
+        <div className="relative mt-1">
+          <div className="absolute top-[50%] left-2 transform translate-y-[-50%]">
+            <SearchIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="h-6 hidden md:inline-flex cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg"
+            alt="progile-photo"
+            className="h-10 w-10 rounded-full object-cover cursor-pointer"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.website-files.com", "upload.wikimedia.org"],
+    domains: [
+      "assets.website-files.com",
+      "upload.wikimedia.org",
+      "techcrunch.com",
+    ],
   },
 };
 
