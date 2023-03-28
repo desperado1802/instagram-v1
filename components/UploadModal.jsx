5aadefe165de3c37b34f6b011f2eda66f96d3dbe
desperado1802 <mumun_ali@hotmail.com>
2023-03-28 12:05:56 +0300

    installed and implemented react modal in the upload modal component

--- components/UploadModal.jsx
+++ components/UploadModal.jsx
@@ -1,9 +1,24 @@
 import { modalState } from "@/atom/modalAtom";
 import React from "react";
 import { useRecoilState } from "recoil";
+import Modal from "react-modal";
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
 
-  return <div>{open && <h1>The modal is open for business</h1>}</div>;
+  return (
+    <div>
+      {open && (
+        <Modal
+          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <h1>Modal</h1>
+          </div>
+        </Modal>
+      )}
+    </div>
+  );
 }
