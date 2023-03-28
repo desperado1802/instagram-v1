50b19738552abff2a9101831c6ff1a4e634aac02
desperado1802 <mumun_ali@hotmail.com>
2023-03-28 14:55:16 +0300

    got data from firestore and reflected it in posts component, so posts now are real data

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,35 +1,31 @@
-import React from "react";
+import { db } from "@/firebase";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 
 export default function Posts() {
-  const posts = [
-    {
-      id: "1",
-      username: "codewithsalim",
-      userImg:
-        "https://cdn.hswstatic.com/gif/play/0b7f4e9b-f59c-4024-9f06-b3dc12850ab7-1920-1080.jpg",
-      img: "https://images.unsplash.com/photo-1679841350010-64f5b144944f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
-      caption: "a lot of fun in the swiss alps",
-    },
-    {
-      id: "2",
-      username: "nazifthelegend",
-      userImg:
-        "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      img: "https://images.unsplash.com/photo-1679843590375-143cc08345df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80",
-      caption: "conquering new lands",
-    },
-  ];
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setPosts(snapshot.docs);
+      }
+    );
+    return unsubscribe;
+  }, []);
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post?.data().username}
+          userImg={post?.data().profileImg}
+          img={post?.data().image}
+          caption={post?.data().caption}
         />
       ))}
     </div>
